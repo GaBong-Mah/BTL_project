15255dc78cf3c529ad5bc77e4b5069509e75038c
GaBong-Mah <tcmah2005@gmail.com>
2026-03-08 23:47:53 +0700

    Delete Database directory

--- Database/data1.sql
+++
@@ -1,7 +0,0 @@
-USE QuanLiChiTieu;
-ALTER TABLE Transactions DROP FOREIGN KEY transactions_ibfk_1;
-ALTER TABLE Wallets MODIFY COLUMN wallet_id VARCHAR(50);
-ALTER TABLE Transactions MODIFY COLUMN wallet_id VARCHAR(50);
-ALTER TABLE Transactions ADD CONSTRAINT fk_wallet_id 
-FOREIGN KEY (wallet_id) REFERENCES Wallets(wallet_id) ON DELETE CASCADE;
-INSERT IGNORE INTO Wallets (wallet_id, wallet_name, balance) VALUES ('1', 'Ví Mô Phỏng', 10000000);
